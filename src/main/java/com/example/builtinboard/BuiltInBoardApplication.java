bc8d7c6491e0461ea5e26a68529f92e239d17a51
jspark <jspark@zalesia.com>
2024-03-06 14:30:07 +0900

    git init

---
+++ src/main/java/com/example/builtinboard/BuiltInBoardApplication.java
@@ -0,0 +1,12 @@
+package com.example.builtinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiltInBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuiltInBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/builtinboard/BuiltInBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.builtinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiltInBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
