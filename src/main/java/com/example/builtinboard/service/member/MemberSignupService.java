8a04b832d3bc32e38c0d796e1bb54196aed4a2a9
jay sung <namic123@naver.com>
2024-03-11 18:01:42 +0900

    회원 가입 로직 완성 및 Member Entity 일부 수정(가입날짜)

--- src/main/java/com/example/builtinboard/service/member/MemberSignupService.java
+++ src/main/java/com/example/builtinboard/service/member/MemberSignupService.java
@@ -14,9 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberSignupService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    public boolean getNickname(String nickname){
+
+    public boolean getNickname(String nickname) {
         // 닉네임이 존재할 경우
-        if(memberRepository.findByNickname(nickname).isPresent()){
+        if (memberRepository.findByNickname(nickname).isPresent()) {
 
             return true;
         }
@@ -26,7 +27,7 @@ public class MemberSignupService {
 
     public boolean getMemberId(String memberId) {
         // 아이디가 존재할 경우
-        if(memberRepository.findByMemberId(memberId).isPresent()){
+        if (memberRepository.findByMemberId(memberId).isPresent()) {
             return true;
         }
         // 존재하지 않을 경우
@@ -35,7 +36,7 @@ public class MemberSignupService {
 
     public boolean getEmail(String email) {
         // 이메일이 존재할 경우
-        if(memberRepository.findByEmail(email).isPresent()){
+        if (memberRepository.findByEmail(email).isPresent()) {
             return true;
         }
         // 존재하지 않는 경우
@@ -46,8 +47,9 @@ public class MemberSignupService {
         try {
             member.setRole(Role.GENERAL_MEMBER);
             member.setPassword(bCryptPasswordEncoder.encode(member.getPassword()));
+            memberRepository.save(member);
             return true;
-        } catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
