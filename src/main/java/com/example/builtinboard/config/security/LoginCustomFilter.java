48c8e731eed0ace33f2f066190163e0e0fefef7d
jay sung <namic123@naver.com>
2024-03-14 11:27:26 +0900

    로그인 검증을 위한 커스텀 필터 로직 작성

---
+++ src/main/java/com/example/builtinboard/config/security/LoginCustomFilter.java
@@ -0,0 +1,48 @@
+package com.example.builtinboard.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+// 로그인 검증을 위한 커스텀 필터
+public class LoginCustomFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public LoginCustomFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+
+
+    }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // 요청에서 username과 password 추출
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        // username과 password 검증을 위해 token에 담는다
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        // authenticationManager로 전달
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    // 로그인 검증 성공 후, 실행 메서드(JWT 발급)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+    // 로그인 검증 실패시, 실행 메서드
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
