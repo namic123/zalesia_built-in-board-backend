89e8d128b4689be0a3be7f3b7eb177eef7e86a3e
jay sung <namic123@naver.com>
2024-03-17 02:20:29 +0900

    오류코드 수정

--- src/main/java/com/example/builtinboard/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/builtinboard/exception/GlobalControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.builtinboard.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -11,23 +12,18 @@ import org.springframework.web.context.request.WebRequest;
 @Slf4j
 public class GlobalControllerExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public void handleException(EntityNotFoundException exception, WebRequest request){
-        log.error("Exception 발생 -> " ,exception);
-        log.error("요청 내용 -> " ,request);
-    }
-
-
     @ExceptionHandler(RuntimeException.class)
-    public void handleRuntimeException(EntityNotFoundException exception, WebRequest request){
+    public ResponseEntity<String> handleRuntimeException(EntityNotFoundException exception, WebRequest request){
         log.error("RuntimeException 발생 -> " ,exception);
         log.error("요청 내용 -> " ,request);
+        return ResponseEntity.internalServerError().body("서버 내부에 오류가 발생했습니다.");
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public void handleEntityNotFoundException(EntityNotFoundException exception, WebRequest request){
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException exception, WebRequest request){
         log.error("EntityNotFoundException 발생 -> " ,exception);
         log.error("요청 내용 -> " ,request);
+        return ResponseEntity.notFound().build();
     }
 
 }
