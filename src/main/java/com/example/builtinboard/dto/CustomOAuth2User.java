5943ccb5a0c5fe6ab39fd1d975d3989c1fd2f202
jay sung <namic123@naver.com>
2024-03-28 11:26:51 +0900

    OAuth2.0 로그인 Provider로부터 반환된 사용자 정보를 나타내는 OAuthUser 인터페이스를 구현한 CustomOAuth2User 클래스 작성

---
+++ src/main/java/com/example/builtinboard/dto/CustomOAuth2User.java
@@ -0,0 +1,45 @@
+package com.example.builtinboard.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+// OAuth 2.0 로그인 프로세스를 통해 인증된 사용자의 정보를 나타냄.
+// 소셜 로그인 Provider로부터 반환된 사용자 속성 정보를 담고 있다.
+public class CustomOAuth2User implements OAuth2User {
+    private final MemberDTO memberDTO;
+
+    public CustomOAuth2User(MemberDTO memberDTO) {
+        this.memberDTO = memberDTO;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return memberDTO.getRole().toString();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return memberDTO.getNickname();
+    }
+    public String getUsername() {
+
+        return memberDTO.getEmail();
+    }
+}
--- src/main/java/com/example/builtinboard/dto/MemberDTO.java
+++ src/main/java/com/example/builtinboard/dto/MemberDTO.java
@@ -5,8 +5,10 @@ import com.example.builtinboard.entity.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class MemberDTO {
     private Long id;
