c4b1b162dba8ba79d9a8b1921cfd8eec3a037420
jay sung <namic123@naver.com>
2024-03-28 10:43:23 +0900

    GoogleResponse Dto 작성

---
+++ src/main/java/com/example/builtinboard/dto/GoogleResponse.java
@@ -0,0 +1,41 @@
+package com.example.builtinboard.dto;
+
+import java.util.Map;
+
+public class GoogleResponse implements OAuth2Response {
+    // 구글 데이터 형식
+//    {
+//        resultcode=00, message=success, id=123123123, name=재성
+//    }
+    private final Map<String, Object> attribute;
+
+    public GoogleResponse(Map<String, Object> attribute) {
+
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+
+        return "google";
+    }
+
+    // 구글의 경우 id를 sub로 가져옴
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
+}
